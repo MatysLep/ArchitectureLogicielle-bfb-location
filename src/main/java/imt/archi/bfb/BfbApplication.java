9ddcbd9393f2b23497bf17c1285c5f003a2e6b75
matys <matys.lepretre@icloud.com>
2025-10-25 21:34:23 +0200

    [add] Spring init

---
+++ src/main/java/imt/archi/bfb/BfbApplication.java
@@ -0,0 +1,13 @@
+package imt.archi.bfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imt/archi/bfb/BfbApplicationTests.java
@@ -0,0 +1,13 @@
+package imt.archi.bfb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
