1b465f87291e110102bef09389670d58d48cf288
ben <benzwd59@gmail.com>
2025-10-28 14:11:05 +0100

    add comment for attributes

--- src/main/java/imt/archi/bfb/vehicles/model/Vehicle.java
+++ src/main/java/imt/archi/bfb/vehicles/model/Vehicle.java
@@ -13,12 +13,40 @@ import java.util.Date;
 @Builder
 @Document(collation = "vehicles")
 public class Vehicle {
+
+    /**
+     * Identifiant unique du véhicule
+     */
     @Id
     public String registration;
+
+    /**
+     * Marque de la voiture
+     */
     public String brand;
+
+    /**
+     * Modèle de la voiture
+     */
     public String model;
+
+    /**
+     * Motorisation de la voiture
+     */
     public String motorization;
+
+    /**
+     * Couleur de la voiture
+     */
     public String color;
+
+    /**
+     * Date d'acquisition
+     */
     public Date acquisitionDate;
+
+    /**
+     * État de la voiture
+     */
     public StateVehicle state;
 }
